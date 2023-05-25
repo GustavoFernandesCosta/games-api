624e13ecd8255aa3c90c6f4f8e7b5536595a726c
Gustavo <gustaferco@gmail.com>
2023-05-25 19:51:30 -0300

    config: install and config prettier

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import express from "express";
-
+import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Word!");
+app.get('/', (req, res) => {
+  res.send('Hello Word!');
 });
 
 app.listen(port, () => {
