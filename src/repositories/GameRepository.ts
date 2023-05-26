292d66590f7ec93c5f7cb45dbd257ecbf2359a19
Gustavo <gustaferco@gmail.com>
2023-05-26 13:03:40 -0300

    commit teste

---
+++ src/repositories/GameRepository.ts
@@ -0,0 +1,19 @@
+import prismaClient from '../../prisma/prismaClient';
+import { IGameDTO } from '../entities/Game';
+import { IGameRepository } from './IGameRepository';
+
+export class GameRepository implements IGameRepository {
+  async register({ releaseData, designer, developer, genre, mode, name, platform }: IGameDTO): Promise<void> {
+    await prismaClient.game.create({
+      data: {
+        releaseData,
+        designer,
+        developer,
+        genre,
+        mode,
+        name,
+        platform
+      }
+    });
+  }
+}
